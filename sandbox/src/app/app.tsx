bd48fc412da8c2826288a5f6a64b0b6429d246a8
extg <extg@yandex-team.ru>
2024-03-28 13:26:10 +0000

    setup sandbox

--- sandbox/src/app/app.tsx
+++ sandbox/src/app/app.tsx
@@ -1,48 +1,86 @@
-import NxWelcome from './nx-welcome';
+import { Button, formatAddress, Popover, PopoverContent, PopoverTrigger } from '@ditto-sdk/shared';
+import { useSDK, MetaMaskProvider } from "@metamask/sdk-react";
+import { Link } from "react-router-dom";
 
-import { Route, Routes, Link } from 'react-router-dom';
+const ConnectWalletButton = () => {
+  const { sdk, connected, connecting, account } = useSDK();
+
+  const connect = async () => {
+    try {
+      await sdk?.connect();
+    } catch (err) {
+      console.warn(`No accounts found`, err);
+    }
+  };
+
+  const disconnect = () => {
+    if (sdk) {
+      sdk.terminate();
+    }
+  };
+
+  return (
+    <div className="relative">
+      {connected ? (
+        <Popover>
+          <PopoverTrigger>
+            <Button>{formatAddress(account)}</Button>
+          </PopoverTrigger>
+          <PopoverContent className="mt-2 w-44 bg-gray-100 border rounded-md shadow-lg right-0 z-10 top-10">
+            <button
+              onClick={disconnect}
+              className="block w-full pl-2 pr-4 py-2 text-left text-[#F05252] hover:bg-gray-200"
+            >
+              Disconnect
+            </button>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Button disabled={connecting} onClick={connect}>
+          Connect Wallet
+        </Button>
+      )}
+    </div>
+  );
+};
+
+const NavBar = () => {
+  const host =
+    typeof window !== "undefined" ? window.location.host : "defaultHost";
+
+  const sdkOptions = {
+    logging: { developerMode: false },
+    checkInstallationImmediately: false,
+    dappMetadata: {
+      name: "Next-Metamask-Boilerplate",
+      url: host, // using the host constant defined above
+    },
+  };
+
+  return (
+    <nav className="flex items-center justify-between max-w-screen-xl px-6 mx-auto py-7 rounded-xl">
+      <Link to="/" className="flex gap-1 px-6">
+        <span className="hidden text-2xl font-bold sm:block">
+          <span className="text-gray-900">Ditto React Example Dapp</span>
+        </span>
+      </Link>
+      <div className="flex gap-4 px-6">
+        <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
+          <ConnectWalletButton />
+        </MetaMaskProvider>
+      </div>
+    </nav>
+  );
+};
+
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="sandbox" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
+    <div className="w-full h-screen">
+      <NavBar />
+      <div className="flex flex-col items-center py-20 justify-center bg-slate-600">
+        <h1 className="text-6xl text-white">Hello World</h1>
       </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
     </div>
   );
 }
