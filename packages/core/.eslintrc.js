7f54f3d8761d61dee6f3754fe7a61ab0522f85f0
Kirill Khramkov <hramkir@gmail.com>
2024-09-26 15:22:11 +0400

    Configure biome

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  root: true,
-  extends: ["@ditto/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+	root: true,
+	extends: ['@ditto/eslint-config/library.js'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+	},
 };
