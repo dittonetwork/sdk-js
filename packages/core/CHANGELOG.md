dde6b26116b102f5a43dcb0a9392854505297ba8
Aleksandr Bakimatov <30955537+abakimatov@users.noreply.github.com>
2025-04-23 18:49:38 +0700

    feat: setup publish

---
+++ packages/core/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @ditto-network/core
+
+## 0.1.0
+
+### Minor Changes
+
+- Base functionality release
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,19 +1,19 @@
-export { createWorkflowBuilder } from './workflow_builder';
-export { createWorkflowSignerBuilder } from './workflow_signer_builder';
-export { createKeplerClient } from './kepler_client';
-export {
-	Step,
-	ContractCallStep,
-	ActionStep,
-	OnChainCallChecker,
-	GasLimitChecker,
-	CountChecker,
-	Checkers,
-	Job,
-	ScheduleTrigger,
-	OnchainEventTrigger,
-	Workflow,
-	AssetPriceChecker,
-	KeplerClient,
-	WorkflowTriggers,
-} from './types';
+export { createWorkflowBuilder } from "./workflow_builder";
+export { createWorkflowSignerBuilder } from "./workflow_signer_builder";
+export { createKeplerClient } from "./kepler_client";
+export type {
+  Step,
+  ContractCallStep,
+  ActionStep,
+  OnChainCallChecker,
+  GasLimitChecker,
+  CountChecker,
+  Checkers,
+  Job,
+  ScheduleTrigger,
+  OnchainEventTrigger,
+  Workflow,
+  AssetPriceChecker,
+  KeplerClient,
+  WorkflowTriggers,
+} from "./types";
